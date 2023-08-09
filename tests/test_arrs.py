b17d26e7f71029683f995beb131344d3f99c42d7
Andrey Potapov <aapot@ya.ru>
2023-08-09 10:44:01 +0300

    add fixtures for all tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,16 +1,15 @@
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 2, "test") == 3
+def test_get(list_array):
+    assert arrs.get(list_array, 2, "test") == 3
     assert arrs.get([], -1, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3, 4], 3, 1) == []
-    assert arrs.my_slice([1, 2, 3, 4], -1) == [4]
-    assert arrs.my_slice([1, 2, 3, 4], -5) == [1, 2, 3, 4]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice(list_array):
+    assert arrs.my_slice(list_array, 1, 3) == [2, 3]
+    assert arrs.my_slice(list_array, 3, 1) == []
+    assert arrs.my_slice(list_array, -1) == [4]
+    assert arrs.my_slice(list_array, -5) == [1, 2, 3, 4]
+    assert arrs.my_slice(list_array, 1) == [2, 3, 4]
     assert arrs.my_slice([], 0) == []
-
--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,8 +1,10 @@
 from utils import dicts
 
 
-def test_get_val():
-    assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
-    assert dicts.get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+def test_get_val(dict_array):
+    assert dicts.get_val(dict_array, "vcs") == "mercurial"
+    assert dicts.get_val(dict_array, "key") == "value"
+    assert dicts.get_val(dict_array, "vcs", "git") == "mercurial"
+    assert dicts.get_val(dict_array, "another_key") == "git"
     assert dicts.get_val({}, "vcs", "git") == "git"
     assert dicts.get_val({}, "vcs", "bazaar") == "bazaar"
